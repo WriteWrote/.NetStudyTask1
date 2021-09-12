2e3924deec048fd500898fb3e62b427dea81c51d
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-09-12 16:19:24 +0300

    added method for spiral-traversing, but it goes only for NxN arrays. Now thinking about recursive spiral traversal for NxM arrays

--- Program.cs
+++ Program.cs
@@ -6,7 +6,57 @@ namespace Task1
 {
   class Program
   {
-    private static List<int> traversalA(int[][] array)
+    private static String ReadFile(String path)
+    {
+      String file = "";
+      if (path != null)
+        if (path.Equals("0") || path.Equals(""))
+        {
+          Console.WriteLine("The default filepath will be used.");
+          file = File.ReadAllText("D:\\XXX\\WorkingFiles\\C#_2021\\Task1\\Task1\\array1.txt");
+        }
+        else
+        {
+          String checkSymbl = path.Split(new char[] {':'}, StringSplitOptions.RemoveEmptyEntries)[0];
+          try
+          {
+            if (checkSymbl.Equals("D") || checkSymbl.Equals("C"))
+            {
+              Console.WriteLine("Succeed");
+              file = File.ReadAllText(path);
+            }
+          }
+          catch (Exception e)
+          {
+            Console.WriteLine(e);
+            Console.WriteLine("Don't do such thing again. For your boldness you ought to rerun the program");
+            throw;
+          }
+        }
+
+      return file;
+    }
+
+    private static int[][] ReadArray(String file)
+    {
+      var lines = file.Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
+      int[][] array = new int[lines.Length][];
+
+      for (int j = 0; j < lines.Length; j++)
+      {
+        var chars = lines[j].Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+        chars[chars.Length - 1] = chars[chars.Length - 1].TrimEnd(new char[] {'\n'});
+        array[j] = new int[chars.Length];
+        for (int i = 0; i < chars.Length; i++)
+        {
+          array[j][i] = Convert.ToInt32(chars[i]);
+        }
+      }
+
+      return array;
+    }
+
+    private static List<int> TraversalA(int[][] array)
     {
       List<int> seq = new List<int>();
       int diff; // for simple sequens (like +diff)
@@ -46,9 +96,51 @@ namespace Task1
       return seq;
     }
 
-    private static bool isOrdered(int[][] array)
+    private static List<int> TraversalC(int[][] array)
     {
-      List<int> travSeq = traversalA(array);
+      List<int> seq = new List<int>();
+      int diff; // for simple sequens (like +diff)
+
+      int n = array.GetLength(0);
+      int count = n;
+      int value = -n;
+      int sum = -1;
+
+      do
+      {
+        value = -1 * value / n;
+        for (int i = 0; i < count; i++)
+        {
+          sum += value;
+          Console.Write(array[sum / n][sum % n] + " ");
+        }
+
+        value *= n;
+        count--;
+        for (int i = 0; i < count; i++)
+        {
+          sum += value;
+          Console.Write(array[sum / n][sum % n] + " ");
+        }
+      } while (count > 0);
+
+      return seq;
+    }
+
+    private static List<int> TraversalB(int[][] array)
+    {
+      List<int> seq = new List<int>();
+      int diff; // for simple sequens (like +diff)
+
+      
+      
+      return seq;
+    }
+
+    private static bool IsOrdered(int[][] array)
+    {
+      //List<int> travSeq = TraversalA(array);
+      List<int> travSeq = TraversalC(array);
       bool isOrdered = false;
       for (int i = 0; i < travSeq.Count - 1; i++)
       {
@@ -59,13 +151,14 @@ namespace Task1
           return false;
         }
       }
+
       return isOrdered;
     }
 
-    private static bool searchSequence(int[][] array)
+    private static bool SearchSequence(int[][] array)
     {
       List<int> seq = new List<int>();
-      int diff; // for simple sequens (like +diff) 
+      int diff; // for simple sequences (like +diff) 
 
       for (int j = 0; j < array[0].Length - 1; j++)
       {
@@ -99,9 +192,9 @@ namespace Task1
         seq.Add(diff);
       }
 
-      foreach (var VARIABLE in seq)
+      foreach (var el in seq)
       {
-        Console.WriteLine(VARIABLE);
+        Console.WriteLine(el);
       }
 
       return false;
@@ -109,56 +202,15 @@ namespace Task1
 
     static void Main(string[] args)
     {
-      // разобраться!!!
-
       Console.WriteLine("Hello World! Enter a filepath. If you don't want to change filepath, type 0:");
       string path = Console.ReadLine();
-      String file = "";
+      String file = ReadFile(path);
 
-      if (path != null)
-        if (path.Equals("0") || path.Equals(""))
-        {
-          Console.WriteLine("The default filepath will be used.");
-          file = File.ReadAllText("D:\\XXX\\WorkingFiles\\C#_2021\\Task1\\Task1\\array1.txt");
-        }
-        else
-        {
-          String checkSymbl = path.Split(new char[] {':'}, StringSplitOptions.RemoveEmptyEntries)[0];
-          try
-          {
-            if (checkSymbl.Equals("D") || checkSymbl.Equals("C"))
-            {
-              Console.WriteLine("Succeed");
-              file = File.ReadAllText(path);
-            }
-          }
-          catch (Exception e)
-          {
-            Console.WriteLine(e);
-            Console.WriteLine("Don't do such thing again. For your boldness you ought to rerun the program");
-            throw;
-          }
-        }
-
-      // taking array out of file.txt
       try
       {
-        var lines = file.Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
-        int[][] array = new int[lines.Length][];
-
-        for (int j = 0; j < lines.Length; j++)
-        {
-          var chars = lines[j].Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
-          chars[chars.Length - 1] = chars[chars.Length - 1].TrimEnd(new char[] {'\n'});
-          array[j] = new int[chars.Length];
-          for (int i = 0; i < chars.Length; i++)
-          {
-            array[j][i] = Convert.ToInt32(chars[i]);
-          }
-        }
-
+        int[][] array = ReadArray(file);
         //searchSequence(array);
-        Console.WriteLine(isOrdered(array));
+        Console.WriteLine(IsOrdered(array));
       }
       catch (Exception e)
       {
